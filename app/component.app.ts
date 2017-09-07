7ed7ca5e27ea4ea3c113b6421028f7f29e02bf91
Rob Wright <rob.wright@groupgti.com>
2017-09-07 10:46:54 +0100

    partials and events

--- app/component.app.ts
+++ app/component.app.ts
@@ -3,9 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   //the element in index.html that will be replaced by this app
   selector: 'app', 
-  templateUrl: '<h1>Artist Directory</h1>'
+  templateUrl: "./partials/app.html"
 })
 
 export class AppComponent {
+  name: string;
+  artists: any;
 
+  onClick(e) {
+    this.name = e.target.innerHTML;
+  }
+
+  addArtist(value) {
+    if(value !=="") {
+      this.artists.push({
+        name: value,
+        school: "Shitarse high"
+      })
+    }
+  }
+
+  constructor() {
+    this.name = "Oon Tah";
+    this.artists = [
+      {
+        name: 'Jizzy Jeff II',
+        school: "Theale Doss"
+      },
+      {
+        name: 'Gaylord Mongewell',
+        school: "Benson Primary School"
+      },
+      {
+        name: 'T-Bone',
+        school: "Crowmarch Whoresons"
+      }
+    ];
+  }
 }
